16e4a9f59924bfff47db0d825088984979938cd9
Ying Qihao <YING2521699035@outlook.com>
2025-08-08 21:37:27 +0800

    补充提交

---
+++ mp_post/src/main/java/com/yqh/mp_post/MpPostApplication.java
@@ -0,0 +1,13 @@
+package com.yqh.mp_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpPostApplication.class, args);
+    }
+
+}
---
+++ mp_post/src/test/java/com/yqh/mp_post/MpPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yqh.mp_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
